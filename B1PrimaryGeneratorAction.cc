27c7529772968b04e51cdf3e52e4b490dc6bf745
jackmcqueen02 <157049725+jackmcqueen02@users.noreply.github.com>
2024-02-06 16:55:43 +0000

    Moved to General Particle source, uniform disk source
    Updated header file to utilise the general particle source

--- B1PrimaryGeneratorAction.cc
+++ B1PrimaryGeneratorAction.cc
@@ -1,33 +1,4 @@
 
-// ********************************************************************
-// * License and Disclaimer                                           *
-// *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
-// * include a list of copyright holders.                             *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
-// * for the full disclaimer and the limitation of liability.         *
-// *                                                                  *
-// * This  code  implementation is the result of  the  scientific and *
-// * technical work of the GEANT4 collaboration.                      *
-// * By using,  copying,  modifying or  distributing the software (or *
-// * any work based  on the software)  you  agree  to acknowledge its *
-// * use  in  resulting  scientific  publications,  and indicate your *
-// * acceptance of all terms of the Geant4 Software license.          *
-// ********************************************************************
-//
-// $Id: B1PrimaryGeneratorAction.cc 94307 2015-11-11 13:42:46Z gcosmo $
-//
-/// \file B1PrimaryGeneratorAction.cc
-/// \brief Implementation of the B1PrimaryGeneratorAction class
-
 #include "B1PrimaryGeneratorAction.hh"
 
 #include "G4LogicalVolumeStore.hh"
@@ -45,57 +16,32 @@
 #include <vector>
 #include <cmath>
 #include <algorithm>
+#include <G4GeneralParticleSource.hh>
 
-
-
-
-
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
+using namespace std;
 
 B1PrimaryGeneratorAction::B1PrimaryGeneratorAction()
-: G4VUserPrimaryGeneratorAction(),
-  fParticleGun(0), 
-  fEnvelopeBox(0)
 {
-
-  // default particle kinematic
-  G4int n_particle = 1000;
-  fParticleGun  = new G4ParticleGun(n_particle);
-  G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
-  G4String particleName;
-  G4ParticleDefinition* particle = particleTable->FindParticle(particleName="gamma");
-  fParticleGun->SetParticleDefinition(particle);
+  fGPS = new G4GeneralParticleSource();
+  G4ParticleDefinition* gamma = G4ParticleTable::GetParticleTable()->FindParticle("gamma"); // gets gamma from source list
+  fGPS->SetParticleDefinition(gamma); // sets particle to gamma
+  fGammaGPS = fGPS->GetCurrentSource();  // shortcut to access source
+  fGammaGPS->GetPosDist()->SetPosDisType("Surface"); // sets the type of source
+  fGammaGPS->GetPosDist()->SetPosDisType("Circle"); // sets the shape of source
+  fGammaGPS->GetPosDist()->SetRadius(1.04*mm); 
   
-  // positioning
-  G4double cosTheta = 2*G4UniformRand() - 1., phi = CLHEP::twopi*G4UniformRand();
-  G4double sinTheta = std::sqrt(1. - cosTheta*cosTheta);
-  G4double ux = sinTheta*std::cos(phi),
-           uy = sinTheta*std::sin(phi),
-           uz = cosTheta;
-  
-  fParticleGun->SetParticleMomentumDirection(G4ThreeVector(ux,uy,uz));
-  fParticleGun->SetParticleEnergy(0*eV);
-  fParticleGun->SetParticlePosition(G4ThreeVector(0.,0.,0.));
-  fParticleGun->SetParticleMomentumDirection(G4ThreeVector(ux,uy,uz));  
-
+  fGammaGPS->GetPosDist()->SetCentreCoords(G4ThreeVector(0., 0., 0.)); // sets particle position
+  fGammaGPS->GetAngDist()->SetAngDistType("iso"); // isotropic source
+  fGammaGPS->GetEneDist()->SetMonoEnergy(0*keV); // defualt energy value
 }
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 B1PrimaryGeneratorAction::~B1PrimaryGeneratorAction()
 {
-  delete fParticleGun;
+  delete fGPS;
 }
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
 
 void B1PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
 {
-    //from data set 1
-    
     // Source Definitions
 
      //Co60 
@@ -105,6 +51,7 @@ void B1PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
                                              826.10, 826.10, 1332.492, 2158.57, 2505.692};
     size_t Co60_size = Co60_intensities.size();
     
+   
     //Cs137
     std::vector<double> Cs137_intensities = {0.91,1.99,3.64,0.348,0.672,0.213,5.8*std::pow(10,-4),85.10};
     
@@ -121,50 +68,35 @@ void B1PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
     
     size_t Ba133_size = Ba133_intensities.size();
     
-    
-    
-    
-    //generates source from vectors
-    
+   
+   
+     // Calculate total intensity
     std::vector<double> cumulative_sums;
     double sum_value = 0;
-
+    // appends the cumalitive sum for the specific emmision
     for (size_t i = 0; i < Co60_size; ++i) {
         double value = Co60_intensities[i];
         sum_value += value;
         cumulative_sums.push_back(sum_value);
     }
-
+    // generates normalised random number for emission selection
     double cumulative_sum = cumulative_sums.back();
     double norm_rand = cumulative_sum * G4UniformRand();
-
+    
+    // source generation
+   
     for (size_t i = 0; i < Co60_size; ++i) {
         if (i == 0 && 0 < norm_rand && norm_rand < Co60_intensities[0]) {
             // Set the energy and exit the function
-            fParticleGun->SetParticleEnergy(Co60_energies[0] * keV);
+            fGammaGPS->GetEneDist()->SetMonoEnergy(Co60_energies[0]*keV);
+
         } 
         
         else if (i > 0 && cumulative_sums[i - 1] < norm_rand && norm_rand < cumulative_sums[i]) {
             // Set the energy and exit the function
-            fParticleGun->SetParticleEnergy(Co60_energies[i] * keV);
+             fGammaGPS->GetEneDist()->SetMonoEnergy(Co60_energies[i]*keV);
         }
     }
-
-
- 
-  // Isotropic Source
-  
-  //Code to cover total solid angle
-  
- G4double cosTheta = 2*G4UniformRand() - 1., phi = CLHEP::twopi*G4UniformRand();
-  G4double sinTheta = std::sqrt(1. - cosTheta*cosTheta);
-  G4double ux = sinTheta*std::cos(phi),
-           uy = sinTheta*std::sin(phi),
-           uz = cosTheta;
-
-// Apply isotropic properties
-
-  fParticleGun->SetParticleMomentumDirection(G4ThreeVector(ux,uy,uz));
-  fParticleGun->GeneratePrimaryVertex(anEvent);
+  // generate the selected event
+  fGPS->GeneratePrimaryVertex(anEvent);
 }
-  
